481ee7b8a08f6fd8e7f2ae94b6102064a1aafdca
lFonseca117 <laufonseca727@gmail.com>
2025-12-12 02:27:12 -0500

    Update apps.py

--- apps/health/apps.py
+++ apps/health/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
+# Configuración de la aplicación Health
 class HealthConfig(AppConfig):
+    # Define el tipo de campo automático por defecto para los IDs de los modelos
     default_auto_field = 'django.db.models.BigAutoField'
+
+    # Nombre de la aplicación dentro del proyecto Django
     name = 'apps.health'
