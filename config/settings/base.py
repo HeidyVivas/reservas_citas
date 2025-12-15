d668b3cd491fb10eeee8bbf7ce6fe09f910c3de7
Heidy-Vivas <hvivas2208@gmail.com>
2025-12-14 22:18:09 -0500

    intento 2

--- config/settings/base.py
+++ config/settings/base.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django_filters',
     
     # Apps locales
-    'apps.users',
+    'apps.users.apps.UsersConfig',
     'apps.citas',
     'apps.core',
 ]
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # ============================================================================
 # MODELO DE USUARIO PERSONALIZADO
 # ============================================================================
-AUTH_USER_MODEL = 'apps.users'   
+AUTH_USER_MODEL = 'users.User'
 
 # ============================================================================
 # INTERNACIONALIZACIÓN
