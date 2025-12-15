90040ec8aa527ecd2fe03f4a928151fefa8eb7f7
Heidy-Vivas <hvivas2208@gmail.com>
2025-12-15 01:27:08 -0500

    intento 8

--- config/settings/base.py
+++ config/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     # Third party apps
     'rest_framework',
     'rest_framework_simplejwt',
+	'django.contrib.staticfiles',
     'drf_yasg',
     'corsheaders',
     'django_filters',
